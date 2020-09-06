d913ef4d2202d64c9c15228c9f23dca01f5e9642
ankit-hindustani <ankit.singh_cs18@gla.ac.in>
2020-09-06 21:54:15 +0530

    starting frontend

---
+++ app.js
@@ -0,0 +1,30 @@
+//jshint esversion:6
+const express= require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended:true
+}))
+
+app.get("/",function(req,res){
+  res.render("home");
+})
+
+app.get("/register",function(req,res){
+  res.render("register");
+})
+
+app.get("/login",function(req,res){
+  res.render("login");
+})
+
+
+
+app.listen(3000,function(){
+  console.log("server start at 3000");
+})
