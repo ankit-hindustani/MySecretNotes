62793033d8c71db9605c1130e7ef0000cd2917aa
ankit-hindustani <ankit.singh_cs18@gla.ac.in>
2020-09-07 15:06:20 +0530

    register users with username and password

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express= require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,6 +11,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended:true
 }))
+
+mongoose.connect("mongodb://localhost:27017",{useNewUrlParser:true,useUnifiedTopology: true});
+const userSchema={
+  email:String,
+  password:String
+}
+const User = new mongoose.model("User",userSchema)
 
 app.get("/",function(req,res){
   res.render("home");
@@ -23,6 +31,40 @@ app.get("/login",function(req,res){
   res.render("login");
 })
 
+app.post("/register",function(req,res){
+  const newUser = new User({
+    email:req.body.username,
+    password:req.body.password
+  });
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login",function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email:username},function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }else{
+          console.log("password not matched");
+        }
+      }else{
+        console.log("user not found");
+      }
+    }
+  });
+});
 
 
 app.listen(3000,function(){
